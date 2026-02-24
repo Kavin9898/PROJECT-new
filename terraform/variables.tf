6c8424fec3ea67249f3d1feaf2d72e3e4e276c76
Kavin9898 <kavindharan40828@gmail.com>
2026-02-24 15:50:54 +0530

    Add Terraform variables for region and instance config

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ami_id" {
+  default = "ami-0df7a207adb9748c7"
+}
+
+variable "key_name" {
+  default = "jenkins"
+}
