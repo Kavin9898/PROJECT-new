b89f117d0a024359a878fb3982aa8db09e6495a5
Kavin9898 <kavindharan40828@gmail.com>
2026-02-24 23:15:10 +0530

    Refactor main.tf to remove unused code and comments
    Removed commented-out sections and cleaned up formatting.

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,11 +11,10 @@ provider "aws" {
   region = var.region
 }
 
-# Dynamic Ubuntu AMI 
 data "aws_ami" "ubuntu" {
   most_recent = true
 
-  owners = ["099720109477"] # Canonical (Ubuntu official)
+  owners = ["099720109477"]
 
   filter {
     name   = "name"
@@ -23,7 +22,6 @@ data "aws_ami" "ubuntu" {
   }
 }
 
-# VPC
 resource "aws_vpc" "instance_vpc" {
   cidr_block = "192.168.0.0/24"
 
@@ -32,7 +30,6 @@ resource "aws_vpc" "instance_vpc" {
   }
 }
 
-# Subnet
 resource "aws_subnet" "instance_subnet" {
   vpc_id                  = aws_vpc.instance_vpc.id
   cidr_block              = "192.168.0.0/25"
@@ -43,7 +40,6 @@ resource "aws_subnet" "instance_subnet" {
   }
 }
 
-# Internet Gateway
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.instance_vpc.id
 
@@ -52,7 +48,6 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
-# Route Table
 resource "aws_route_table" "instance_route" {
   vpc_id = aws_vpc.instance_vpc.id
 
@@ -66,13 +61,11 @@ resource "aws_route_table" "instance_route" {
   }
 }
 
-# Route Table Association
 resource "aws_route_table_association" "rt_assoc" {
   subnet_id      = aws_subnet.instance_subnet.id
   route_table_id = aws_route_table.instance_route.id
 }
 
-# Security Group
 resource "aws_security_group" "instance_sg" {
   name   = "instance-sg"
   vpc_id = aws_vpc.instance_vpc.id
@@ -99,120 +92,8 @@ resource "aws_security_group" "instance_sg" {
   }
 }
 
-# EC2 Instance
 resource "aws_instance" "my_instance" {
-  ami           = data.aws_ami.ubuntu.id   # ✅ FIXED
-  instance_type = var.instance_type
-
-  subnet_id              = aws_subnet.instance_subnet.id
-  vpc_security_group_ids = [aws_security_group.instance_sg.id]
-
-  associate_public_ip_address = true
-  key_name = var.key_name
-
-  user_data = <<-EOF
-#!/bin/bash
-sudo apt update -y
-sudo apt install nginx -y
-sudo systemctl start nginx
-sudo systemctl enable nginx
-EOF
-
-  tags = {
-    Name = "Terraform-Instance"
-  }
-}terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.region
-}
-
-# VPC
-resource "aws_vpc" "instance_vpc" {
-  cidr_block = "192.168.0.0/24"
-
-  tags = {
-    Name = "instance-vpc"
-  }
-}
-
-# Subnet
-resource "aws_subnet" "instance_subnet" {
-  vpc_id                  = aws_vpc.instance_vpc.id
-  cidr_block              = "192.168.0.0/25"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "instance-subnet"
-  }
-}
-
-# Internet Gateway
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.instance_vpc.id
-
-  tags = {
-    Name = "instance-igw"
-  }
-}
-
-# Route Table
-resource "aws_route_table" "instance_route" {
-  vpc_id = aws_vpc.instance_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  tags = {
-    Name = "instance-route"
-  }
-}
-
-# Route Table Association
-resource "aws_route_table_association" "rt_assoc" {
-  subnet_id      = aws_subnet.instance_subnet.id
-  route_table_id = aws_route_table.instance_route.id
-}
-
-# Security Group
-resource "aws_security_group" "instance_sg" {
-  name   = "instance-sg"
-  vpc_id = aws_vpc.instance_vpc.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# EC2 Instance
-resource "aws_instance" "my_instance" {
-  ami           = var.ami_id
+  ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
 
   subnet_id              = aws_subnet.instance_subnet.id
